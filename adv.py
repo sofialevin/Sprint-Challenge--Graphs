45789ad45216bb41c8e9c7143caef0583524fdc3
Sofia Levin <sofiafontes@me.com>
2020-04-17 10:59:20 -0700

    Non-working attempt.

--- adv.py
+++ adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Stack, Queue
 
 import random
 from ast import literal_eval
@@ -29,7 +30,70 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+visited = {}
+prev_room = None
 
+rooms = set()
+
+# helper function for getting opposite direction
+def opposite(direction):
+    if direction == 'n':
+        return 's'
+    elif direction == 's':
+        return 'n'
+    elif direction == 'e':
+        return 'w'
+    else:
+        return 'e'
+
+visited = {0: {'n': '?', 's': '?', 'e': '?', 'w': '?'}}
+
+directions = player.current_room.get_exits()
+current_direction = directions[0]
+
+unexplored_rooms = len(room_graph)
+
+while unexplored_rooms > 0:
+    prev_room = player.current_room.id
+    player.travel(current_direction)
+    traversal_path.append(current_direction)
+    rooms.add(player.current_room.id)
+
+    visited[prev_room][current_direction] = player.current_room.id
+
+    if player.current_room.id not in visited:
+        visited[player.current_room.id] = {}
+        visited[player.current_room.id][opposite(current_direction)] = prev_room
+
+    current_direction = None
+    
+    unexplored_exits = 0
+    for direction in player.current_room.get_exits():
+        if direction not in visited[player.current_room.id]:
+            current_direction = direction
+            visited[player.current_room.id][direction] = "?"
+            unexplored_exits += 1
+
+    if unexplored_exits == 0:
+
+        queue = Queue()
+        explored = set()
+
+        queue.enqueue(player.current_room.get_exits()[0])
+
+        while queue.size() > 0 and current_direction == None:
+            path = queue.dequeue()
+            new_direction = path[-1]
+            player.travel(new_direction)
+            traversal_path.append(new_direction)
+            if player.current_room.id not in explored:
+                explored.add(player.current_room.id)
+
+                for door in player.current_room.get_exits():
+                    if visited[player.current_room.id][door] == "?":
+                        current_direction = door
+                    else:
+                        queue.enqueue(new_direction)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -38,7 +102,7 @@ visited_rooms.add(player.current_room)
 
 for move in traversal_path:
     player.travel(move)
-    visited_rooms.add(player.current_room)
+    visited_rooms.add(player.current_room.id)
 
 if len(visited_rooms) == len(room_graph):
     print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
@@ -51,12 +115,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
